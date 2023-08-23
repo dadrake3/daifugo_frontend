59ee1027329a261385c3207992568cbfb28e49eb
rarearth <dadrake3@gmail.com>
2023-08-23 15:29:59 -0500

    add basic stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { gql, useSubscription } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
+
+
+const CREATE_GAME = gql`
+    mutation CreateGame {
+      createGame {
+        id
+        state_id
+        joinable
+        players
+      }
+    }
+  `;
+
+// const UPDATED_HANDS = gql`
+//     subscription OnUpdatedHand($id: ID!) {
+//       updatedHand(id: $id) {
+//         id
+//         cards
+//       }
+//     }
+  // `;
+const UPDATED_HANDS = gql`
+    subscription OnUpdatedHand {
+      updatedHand(id: "64d48e5c-ab78-48c4-a907-250a5c84f5ab") {
+        id
+        cards
+      }
+    }
+  `;
+
+function MyComponent() {
+  // const [createGame, { data }] = useMutation(CREATE_GAME);
+  const hand_id = "64d48e5c-ab78-48c4-a907-250a5c84f5ab"
+  const { data, loading } = useSubscription(
+    UPDATED_HANDS, 
+    { variables: { hand_id } }
+  );
+
+  if (data) {console.log(data)};
+  // if (data) {console.log(data)};
+  if (loading) return "data not fetched yet";
+  // return <button onClick={createGame}>Click Me!</button>
+  // return <h4>Fuck {data.updatedHand.id}</h4>;
+  return (<ol>
+      {data.updatedHand.cards.map(item => <li>{item}</li>)}
+  </ol>)
+
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      {MyComponent()}
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,55 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createAuthLink } from "aws-appsync-auth-link";
+import { createSubscriptionHandshakeLink } from "aws-appsync-subscription-link";
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+} from "@apollo/client";
+
+import appSyncConfig from "./aws-exports";
+
+
+const url = appSyncConfig.aws_appsync_graphqlEndpoint;
+const region = appSyncConfig.aws_appsync_region;
+
+const auth = {
+  type: appSyncConfig.aws_appsync_authenticationType,
+  apiKey: appSyncConfig.aws_appsync_apiKey,
+  // jwtToken: async () => token, // Required when you use Cognito UserPools OR OpenID Connect. token object is obtained previously
+  // credentials: async () => credentials, // Required when you use IAM-based auth.
+};
+
+const httpLink = new HttpLink({ uri: url });
+
+const link = ApolloLink.from([
+  createAuthLink({ url, region, auth }),
+  createSubscriptionHandshakeLink({ url, region, auth }, httpLink),
+]);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>,
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
